9618b638a9a1692a29931d6011668a454f2342a3
michal sagi <michaldinner@gmail.com>
2022-06-27 09:39:35 +0300

    michalmonday

--- src/components/layout/Popup/Popup.js
+++ src/components/layout/Popup/Popup.js
@@ -1,8 +1,12 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Popup.css';
+// import styles from './style.module.css';
+
 
 export default function Popup() {
+
+    let nameBook = { name: "HarryPotter", gener: "roman" }
 
     const [popup, setPopup] = useState(true)
     const navigate = useNavigate()
@@ -15,13 +19,25 @@ export default function Popup() {
         navigate('/bookInfo')
     }
 
-    return <div
-        className={`overlay ${popup ? '' : 'close'}`}
-    >
-        <div className='popup'>
-            <button onClick={() => yes()}>yes</button>
-            <button onClick={() => no()}  >No</button>
-        </div>
+    return (
+        <>
+            <div
+                className={`overlay ${popup ? '' : 'close'}`} >
+                <div className='backpopup'>    </div>
+                <div className='popup '>
+                    <div className='title'>Reading Exercise</div>
+                    <div className='txt'>Are you still reading {nameBook.name}?</div>
+                    <div className='btn'>
+                        <button className='btn_yes' onClick={() => yes()}>Yes</button>
+                        <button className='btn_no' onClick={() => no()}  >No</button>
+                    </div>
+                </div>
 
-    </div>
+            </div>
+
+
+
+
+        </>
+    )
 }
